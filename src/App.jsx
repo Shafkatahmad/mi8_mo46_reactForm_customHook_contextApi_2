3630a1985f5432cf1123862639ea14c84e78ffd6
Shafkatahmad <shafkataahmad@gmail.com>
2026-03-09 20:45:33 +0600

    reusable form using props and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import HookForm from './Components/HookForm/HookForm'
 import RefForm from './Components/RefForm/RefForm'
+import ReuseableForm from './Components/ReuseableForm/ReuseableForm'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 import StateFulForm from './Components/StateFulForm/StateFulForm'
 
 function App() {
+
+  const handleSignUpSubmit = data => {
+    console.log('Sign Up data: ', data);
+  }
+
+  const handleUpdateProfile = data => {
+    console.log('Update Profile: ', data);
+  }
 
   return (
     <>
@@ -12,7 +21,26 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFulForm></StateFulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReuseableForm
+          formTitle={'Sign Up'}
+          handleSubmit={handleSignUpSubmit}
+      >
+        <div>
+          <h2>Sign Up</h2>
+          <p>Please Sign Up right now</p>
+        </div>
+      </ReuseableForm>
+      <ReuseableForm
+          formTitle={'Profile Update'}
+          handleSubmit={handleUpdateProfile}
+          submitBtnText="Update"
+      >
+        <div>
+          <h2>Update Profile</h2>
+          <p>Always keep your profile updated</p>
+        </div>
+      </ReuseableForm>
     </>
   )
 }
