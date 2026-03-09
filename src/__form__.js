0ee4f60cb50df57d60932a5df12ee887985faa4d
Shafkatahmad <shafkataahmad@gmail.com>
2026-03-09 13:25:08 +0600

    form with state to get value, set defalut value, error handling

--- src/__form__.js
+++ src/__form__.js
@@ -1,3 +1,5 @@
 /**
- *  1. Use form tag and name to get values
+ *1. Use form tag and onSubmit handler with event (e). From e, access e.target.[name of the input field].value and name to get values
+ * 
+ * 2. Use individual state
  */
